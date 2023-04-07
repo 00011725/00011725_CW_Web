d8edf31f1a3cbc7807f25a4e84dd76a6134dbcbf
00011725 <91447450+00011725@users.noreply.github.com>
2023-04-07 20:46:32 +0500

    Add files via upload

---
+++ WebApi/DatabaseAccess/SqlQuery_1.sql
@@ -0,0 +1,13 @@
+﻿insert into Books([BookName],[Author],[Replacement],[ForRent],[PublitionYear])
+values ('Alice', 'J.Carrol','3 floor','1','12-10-12 12:32:10.1234567')
+
+insert into Books([BookName],[Author],[Replacement],[ForRent],[PublitionYear])
+values ('H POTTER', 'J.Rowling','2 floor','0','11-10-12 11:32:10.1234567')
+
+
+insert into Rents([BookName],[RentState],[OrderDate])
+values ('Alice','1','08-11-13 12:32:10.1234567')
+
+insert into Rents([BookName],[RentState],[OrderDate])
+values ('Jack','0','07-11-11 07:39:24.1234567')
+
